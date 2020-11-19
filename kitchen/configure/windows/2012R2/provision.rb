0113da9e9be65d53461820b39523578c4a5c2fb0
Gareth Evans <gareth.evans@contino.io>
2020-11-19 14:27:31 +0000

    Initial version of the role.

--- kitchen/configure/windows/2012R2/provision.rb
+++ kitchen/configure/windows/2012R2/provision.rb
@@ -1,10 +1,5 @@
 Vagrant.configure(2) do |config|
   config.vm.provision "shell", inline: <<-SHELL
-    Set-ExecutionPolicy Unrestricted
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1'))
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    # choco install ruby -y
-    # choco install powershell -y
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1'))    
   SHELL
 end
--- kitchen/test/integration/default/default.rb
+++ kitchen/test/integration/default/default.rb
@@ -4,3 +4,37 @@ describe file('C:/Windows/explorer.exe') do
   it { should exist }
   it { should be_file }
 end
+
+describe service("WinRM") do
+  it { should be_installed }
+  it { should be_running }
+end
+
+describe service("Telnet") do
+  it { should_not be_installed }
+end
+
+describe port(23) do
+  it { should_not be_listening }
+end
+
+describe port(80) do
+  it { should_not be_listening }
+end
+
+describe port(443) do
+  it { should_not be_listening }
+end
+
+describe port(3389) do
+  it { should be_listening }
+end
+
+# look for all administrators: SID: S-1-5-21domain-500
+describe users.where { uid =~ /S\-1\-5\-21\-\d+\-\d+\-\d+\-500/ } do
+  it { should exist }
+end
+
+describe chocolatey_package('baretail') do
+  it { should be_installed }
+end
